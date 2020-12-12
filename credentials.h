c5551bb8f701c3f7f7dfd6bdf8bf8bcd02b171d0
Mingye Chen <61096358+mingyeeee@users.noreply.github.com>
2020-12-12 00:02:23 -0500

    Create credentials.h

---
+++ credentials.h
@@ -0,0 +1,5 @@
+#define mySSID "Your Wifi SSID"
+#define myPASSWORD "Your Wifi password"
+
+#define GITHUB_PROFILE_LINK_1 "Thingspeak API url for this week's github activity data" 
+#define GITHUB_PROFILE_LINK_2 "Thingspeak API url for last week's github activity data" 
